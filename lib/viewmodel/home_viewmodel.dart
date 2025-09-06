262dc2ad38cb8946666494a805d664291df83073
KTH <pni2396@gmail.com>
2025-09-06 23:33:20 +0900

    feat: 임시 push

--- lib/viewmodel/home_viewmodel.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:futurefinder_flutter/mixin/loading_mixin.dart';
-import 'package:futurefinder_flutter/repository/user_repository.dart';
-
-class HomeViewModel extends ChangeNotifier with LoadingMixin {
-  final UserRepository _userRepository;
-
-  HomeViewModel(this._userRepository);
-}
